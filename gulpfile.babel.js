46a60208e2e39e748e01edfe8332593eec81ab3a
wangchi <hiwangchi@gmail.com>
2015-12-12 11:38:38 +0800

    eslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
-import gulp from 'gulp';
-import less from 'gulp-less';
+import gulp from 'gulp'
+import less from 'gulp-less'
 
 const paths = {
   less: 'less/',
   dest: 'build/'
-};
+}
 
 gulp.task('less', () => {
   gulp.src(`${paths.less}**/*.less`)
     .pipe(less())
-    .pipe(gulp.dest(`${paths.dest}css/`));
-});
+    .pipe(gulp.dest(`${paths.dest}css/`))
+})
